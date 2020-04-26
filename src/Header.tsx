df49ab59c06c6df7a9a1df5a2d73213b00d5ee4f
galudisu <galudisu@gmail.com>
2020-04-26 23:12:51 +0800

    query graphQL

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,6 +3,45 @@
 import React from 'react';
 import axios from 'axios';
 
+interface IViewer {
+  name: string;
+  avatarUrl: string;
+}
+
+interface IQueryResult {
+  data: {
+    viewer: IViewer;
+  };
+}
 export const Header: React.FC = () => {
-  return null;
+  const [viewer, setViewer]: [IViewer, (viewer: IViewer) => void] = React.useState({name: '', avatarUrl: ''});
+
+  React.useEffect(() => {
+    axios.post<IQueryResult>(
+      'https://api.github.com/graphql',
+      {
+        query: `query {
+          viewer {
+          name
+          avatarUrl
+          }
+        }`
+      },
+      {
+        headers: {
+          Authorization: 'bearer b840dacb1ba82f83bb510564746a5c3edb32c970',
+        },
+      },
+    ).then(response => {
+      setViewer(response.data.data.viewer);
+    });
+  }, []);
+
+  return (
+    <div>
+      <img src={viewer.avatarUrl} className="avatar" />
+      <div className="viewer">{viewer.name}</div>
+      <h1>GitHub Search</h1>
+    </div>
+  );
 };
